59e5d8144d4be6e29220ab2548b5915195578b5c
TarzanKeyz <dannybedard1@gmail.com>
2020-01-24 10:56:57 -0500

    more more!

--- Tableaux.java
+++ Tableaux.java
@@ -1,7 +1,7 @@
 /**
  * @author France Beaudoin
  */
-//asdfsdfsadfsdf
+
 public class Tableaux {
     private static void permute(int[] tab, int i1, int i2){
         int transit = tab[i1];
@@ -39,5 +39,71 @@ public class Tableaux {
             if (imin != i)
                 permute(tab, i, imin);
         }
+    }
+
+    public static void trierDecroissant(int[] tab){
+        int imax;
+        for (int i=0;i<tab.length-1;i++){
+            imax = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (tab[j] > tab[imax])
+                    imax = j;
+            }
+            if (imax != i){
+                permute(tab, i, imax);
+            }
+        }
+    }
+
+    public static void print (int [] tab){
+        for (int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+    }
+
+    public static int maximum (int [] tab){
+        int max = tab[0];
+        for(int i=0; i<tab.length-1; i++){
+            if(tab[i] > max)
+                max = tab[i];
+        }
+        return max;
+    }
+
+    public static int minimum (int [] tab){
+        int min = tab [0];
+        for (int i=0; i<tab.length;i++){
+            if (tab[i] < min)
+                min = tab[i];
+        }
+        return min;
+    }
+
+    public static double moyenne (double [] tab){
+        double total = 0.0;
+        double moyenne;
+
+        for (int i=0; i<tab.length; i++){
+            total = total + tab[i];
+        }
+        moyenne = total / tab.length;
+        return moyenne;
+    }
+
+    public static int fouilleSeq (int [] tab, int cherche){
+        boolean trouve = false;
+        int parcours = 0;
+
+        while (parcours < tab.length && !trouve){
+            if (tab[parcours] == cherche)
+                trouve = true;
+            else
+            parcours ++;
+        }
+        if (trouve)
+            return parcours;
+        else
+            return -1;
+
     }
 }
